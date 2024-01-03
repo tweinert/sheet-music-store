8c946350eac1f4cafa9a0c509fa889536d25463a
Tait Weinert <tait.weinert@outlook.com>
2024-01-03 13:35:35 +1030

    updated song schema

--- models/song.js
+++ models/song.js
@@ -5,7 +5,12 @@ const Schema = mongoose.Schema;
 const SongSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   composer: { type: Schema.Types.ObjectId, ref: "Composer", required: true },
-  difficulty: { type: String, required: true, maxLength: 100 },
+  difficulty: { 
+    type: String,
+    required: true,
+    enum: ["Beginner", "Intermediate", "Advanced" ],
+    default: "Beginner",
+  },
   instrument: [{ type: Schema.Types.ObjectId, ref: "Instrument"}],
   period: { type: Schema.Types.ObjectId, ref: "Period"},
   price: { type: Number, required: true },
