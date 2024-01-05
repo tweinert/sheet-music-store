85501fc7bf3e1ccb38f54d5cf271a366db955f85
Tait Weinert <tait.weinert@outlook.com>
2024-01-05 13:04:07 +1030

    populatedb complete

--- populatedb.js
+++ populatedb.js
@@ -36,9 +36,6 @@ async function main() {
   mongoose.connection.close();
 }
 
-// We pass the index to the ...Create functions so that, for example,
-// genre[0] will always be the Fantasy genre, regardless of the order
-// in which the elements of promise.all's argument complete.
 async function periodCreate(index, name) {
   const period = new Period({ name: name });
   await period.save();
@@ -93,8 +90,8 @@ async function songCreate(index, name, composer, difficulty, price,
 async function createPeriods() {
   console.log("Adding periods");
   await Promise.all([
-    periodCreate(0, "Classical"),
-    periodCreate(1, "Baroque"),
+    periodCreate(0, "Baroque"),
+    periodCreate(1, "Classical"),
     periodCreate(2, "Romantic"),
   ]);
 }
@@ -119,122 +116,72 @@ async function createComposers() {
     composerCreate(3, "Wolfgang", "Mozart", "Austrian", "1756-01-27", "1791-12-05"),
     composerCreate(4, "Pyotr", "Tchaikovsky", "Russian", false, false),
     composerCreate(5, "Antonio", "Vivaldi", "Italian", "1678-03-04", "1741-07-28"),
+    composerCreate(6, "Test", "Composer", false, false, false),
   ]);
 }
 
 async function createSongs() {
   console.log("Adding songs");
   await Promise.all([
-    songCreate(0, "name",
+    songCreate(0,
+      "Cello Suite no. 1 in G major",
       composers[0],
       "Beginner",
       9.95,
       10,
-      [instruments[0]],
+      [instruments[3]],
       periods[0],
       false
     ),
-  ]);
-}
-
-async function createBooks() {
-  console.log("Adding Books");
-  await Promise.all([
-    bookCreate(0,
-      "The Name of the Wind (The Kingkiller Chronicle, #1)",
-      "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
-      "9781473211896",
-      authors[0],
-      [genres[0]]
-    ),
-    bookCreate(1,
-      "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-      "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-      "9788401352836",
-      authors[0],
-      [genres[0]]
-    ),
-    bookCreate(2,
-      "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-      "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-      "9780756411336",
-      authors[0],
-      [genres[0]]
-    ),
-    bookCreate(3,
-      "Apes and Angels",
-      "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-      "9780765379528",
-      authors[1],
-      [genres[1]]
-    ),
-    bookCreate(4,
-      "Death Wave",
-      "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-      "9780765379504",
-      authors[1],
-      [genres[1]]
-    ),
-    bookCreate(5,
-      "Test Book 1",
-      "Summary of test book 1",
-      "ISBN111111",
-      authors[4],
-      [genres[0], genres[1]]
-    ),
-    bookCreate(6,
-      "Test Book 2",
-      "Summary of test book 2",
-      "ISBN222222",
-      authors[4],
+    songCreate(1,
+      "Moonlight Sonata",
+      composers[1],
+      "Intermediate",
+      15.00,
+      5,
+      [instruments[0]],
+      periods[1],
       false
     ),
-  ]);
-}
-
-async function createBookInstances() {
-  console.log("Adding authors");
-  await Promise.all([
-    bookInstanceCreate(0, books[0], "London Gollancz, 2014.", false, "Available"),
-    bookInstanceCreate(1, books[1], " Gollancz, 2011.", false, "Loaned"),
-    bookInstanceCreate(2, books[2], " Gollancz, 2015.", false, false),
-    bookInstanceCreate(3,
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available"
+    songCreate(2,
+      "Symphony No. 5 in C minor",
+      composers[1],
+      "Intermediate",
+      21.95,
+      12,
+      [instruments[1], instruments[3], instruments[4]],
+      periods[1],
+      false
     ),
-    bookInstanceCreate(4,
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available"
+    songCreate(3,
+      "Nocturne no. 20 in C-Sharp Minor",
+      composers[2],
+      "Intermediate",
+      12.50,
+      23,
+      [instruments[0]],
+      periods[2],
+      false
     ),
-    bookInstanceCreate(5,
-      books[3],
-      "New York Tom Doherty Associates, 2016.",
-      false,
-      "Available"
+    songCreate(4,
+      "Concerto No. 4 in F Minor",
+      composers[5],
+      "Advanced",
+      8.95,
+      5,
+      [instruments[1]],
+      periods[0],
+      false
     ),
-    bookInstanceCreate(6,
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
+    songCreate(5,
+      "Test Song 1",
+      composers[6],
+      "Beginner",
+      0.00,
+      0,
       false,
-      "Available"
-    ),
-    bookInstanceCreate(7,
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
       false,
-      "Maintenance"
+      false
     ),
-    bookInstanceCreate(8,
-      books[4],
-      "New York, NY Tom Doherty Associates, LLC, 2015.",
-      false,
-      "Loaned"
-    ),
-    bookInstanceCreate(9, books[0], "Imprint XXX2", false, false),
-    bookInstanceCreate(10, books[1], "Imprint XXX3", false, false),
   ]);
 }
